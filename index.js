6ddf5af95c827d51a19a4cb28debab0cb5b7b52a
Angelica Soto <angelica.soto.k@hotmail.com>
2024-11-23 20:25:37 -0300

    proyecto 1 veterinaria node

---
+++ index.js
@@ -0,0 +1,18 @@
+const { registrar, leer } = require("./operaciones.js");
+
+const argumentos = process.argv.slice(2);
+const operacion = argumentos[0];
+
+if (operacion === "registrar") {
+  registrar(
+    argumentos[1],
+    argumentos[2],
+    argumentos[3],
+    argumentos[4],
+    argumentos[5]
+  );
+}
+
+if (operacion === "leer") {
+  leer();
+}
